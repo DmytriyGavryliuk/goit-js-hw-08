306846da57993d725a9334d31ac514e5ce725d98
DmytriyGavryliuk <114095666+DmytriyGavryliuk@users.noreply.github.com>
2023-02-25 21:49:07 +0200

    1
    1

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,23 @@
+import throttle from 'lodash.throttle';
 
+
+const mailEl = document.querySelector('input[type="email"]');
+const textAreaEl = document.querySelector('textarea[name="message"]');
+const formEl = document.querySelector('form.feedback-form');
+
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+let saveDataMail;
+let saceDataMessage;
+const data = {
+  email: '',
+  message: '',
+};
+
+
+formEl.addEventListener("input", throttle(onInputStorage, 500));
+
+function onInputStorage() {
+    data.email = mailEl.value;
+    data.message = textAreaEl.value;
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(data));
+}
