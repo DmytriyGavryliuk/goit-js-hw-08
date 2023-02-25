06f4e7ffdc9e036888bd81209a9fae65069367dc
DmytriyGavryliuk <114095666+DmytriyGavryliuk@users.noreply.github.com>
2023-02-25 22:22:34 +0200

    2
    2

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,3 +1,4 @@
+
 import throttle from 'lodash.throttle';
 
 
@@ -6,18 +7,45 @@ const textAreaEl = document.querySelector('textarea[name="message"]');
 const formEl = document.querySelector('form.feedback-form');
 
 const LOCALSTORAGE_KEY = 'feedback-form-state';
-let saveDataMail;
-let saceDataMessage;
 const data = {
   email: '',
   message: '',
 };
-
+getData()
 
 formEl.addEventListener("input", throttle(onInputStorage, 500));
+formEl.addEventListener("submit", onSubmit);
 
 function onInputStorage() {
     data.email = mailEl.value;
     data.message = textAreaEl.value;
     localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(data));
 }
+
+
+function getData() {
+  if (JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)) === null) { 
+    mailEl.value = "";
+    textAreaEl.value = "";
+  } else {
+  let loadData = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+    mailEl.value = loadData.email;
+    textAreaEl.value = loadData.message;
+    data.email = loadData.email;
+    data.message = loadData.message;
+  }
+}
+
+
+function onSubmit(e) {
+  e.preventDefault();
+  console.log(`email =>`, data.email);
+  console.log(`massage =>`, data.message);
+  data.email = '';
+  data.message = '';
+  localStorage.removeItem(LOCALSTORAGE_KEY);
+  mailEl.value = '';
+  textAreaEl.value = '';
+
+}
+
